702d261a8181936b4d545c901473a5013a8d86c8
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2025-03-02 10:52:25 +0530

    Add files via upload

---
+++ perfectnumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        int n = S.nextInt();
+        int sum = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum = sum + i;
+            }
+        }
+        if (sum == n) {
+            System.out.println("Perfect number");
+        } else {
+            System.out.println("Not a perfect number");
+        }
+    }
+}
